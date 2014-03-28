fd5c1bb116e040a2cbaf406422ba61f203b088ae
tatiannurs <tatiannaxshoals@gmail.com>
2014-03-28 08:17:56 -0700

    Initial Commit
    The first commit of melonJS platform style game.

---
+++ public_html/js/resources.js
@@ -0,0 +1,29 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+        {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
+
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+           {name: "level1", type: "tmx", src: "data/map/level1.tmx"}
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+];
